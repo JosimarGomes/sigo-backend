6fe8e13d7f8094cc8f155ff08ae83d4be4d89180
Josimar Gomes <josimar.gomes@superlogica.com>
2020-08-15 23:44:24 -0300

    adjust database

--- server/modules/consulting/configs/database.js
+++ server/modules/consulting/configs/database.js
@@ -5,7 +5,7 @@ module.exports = {
 
     database: null,
     getConnection: () => {
-        this.database = mysql.createConnection('mysql://b07adeaefe7531:b7d05715@us-cdbr-east-02.cleardb.com/heroku_d6bdf658ab39625?reconnect=true');       
+        this.database = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);       
     },
     query: query => {
 
