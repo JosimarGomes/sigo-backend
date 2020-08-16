e919ebd36159cb147df229afb0e5be4a48c43554
Josimar Gomes <josimar.gomes@superlogica.com>
2020-08-15 21:39:31 -0300

    adjust database

--- server/modules/consulting/configs/database.js
+++ server/modules/consulting/configs/database.js
@@ -1,6 +1,6 @@
 const mysql      = require('mysql');
 const connection = mysql.createConnection({
-    host     : 'localhost',
+    host     : '',
     user     : 'root',
     password : 'password',
     database : 'sigo-consultorias'
